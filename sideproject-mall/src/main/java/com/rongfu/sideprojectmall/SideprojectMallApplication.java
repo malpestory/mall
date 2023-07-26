957fdf9b0cb853f5fcd192723f3104c6f50ddb8f
malpestory <go02849854@Gmail.com>
2023-07-26 18:59:39 +0800

    初始化spring boot

---
+++ sideproject-mall/src/main/java/com/rongfu/sideprojectmall/SideprojectMallApplication.java
@@ -0,0 +1,13 @@
+package com.rongfu.sideprojectmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideprojectMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideprojectMallApplication.class, args);
+	}
+
+}
---
+++ sideproject-mall/src/test/java/com/rongfu/sideprojectmall/SideprojectMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rongfu.sideprojectmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
